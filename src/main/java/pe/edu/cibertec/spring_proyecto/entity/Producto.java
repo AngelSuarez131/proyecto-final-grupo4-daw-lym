7bda257b5d07f45f07518d0a07b4fc23b91cd635
stephaniee04 <stephanieeusebiogarro@gmail.com>
2024-12-13 15:17:33 -0500

    listado y detalles de productos hechos

--- src/main/java/pe/edu/cibertec/spring_proyecto/entity/Producto.java
+++ src/main/java/pe/edu/cibertec/spring_proyecto/entity/Producto.java
@@ -1,9 +1,6 @@
 package pe.edu.cibertec.spring_proyecto.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,4 +18,6 @@ public class Producto {
     private String uniMedida;
     private Integer stock;
     private String marca;
+
+
 }
